34b3eca5c63cd414cfc208a4ab3f6a7aa2787876
eduardo.batista <eduardorochabat@gmail.com>
2023-10-18 11:02:48 -0300

    Criação de rotas 🖇️

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EquipmentTypeController;
+use App\Http\Controllers\EquipmentController;
+use App\Http\Controllers\CollaboratorController;
+use App\Http\Controllers\LoanTextController;
+use App\Http\Controllers\EquipmentLoanController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +22,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// System routes
+Route::resource('equipmenttypes', EquipmentTypeController::class);
+Route::resource('equipments', EquipmentController::class);
+Route::resource('collaborators', CollaboratorController::class);
+Route::resource('loantexts', LoanTextController::class);
+Route::resource('equipmentloans', EquipmentLoanController::class);
+Route::resource('user', UserController::class);
